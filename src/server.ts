cc40c3b0f8e65c464c5c1781a2643ba85504d3ce
Kaif-Sasoli <kaifsasoli1@gmail.com>
2026-09-25 17:22:45 +0500

    job scheduler commentted

--- src/server.ts
+++ src/server.ts
@@ -27,11 +27,11 @@ import weatherRoutes from './routes/weather.routes.js'
 const app = express();
 
 // Middlewares
-app.use(helmet());
+// app.use(helmet());
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // Test
 app.get("/", (req, res) => {
@@ -85,7 +85,7 @@ app.use("/weather", weatherRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} ... `);
-    // startNotificationJob();
+    startNotificationJob();
 });
 
 export default app;
